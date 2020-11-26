e515cc1b15d8cd9d17fc4080c8dcc2aa03b32ecf
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-26 10:06:43 -0300

    fix: removed unused import

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,6 @@ import Layout from './components/Layout';
 
 import { ThemeProvider as ThemeConsumer } from './context/theme';
 
-import 'react-toggle/style.css';
 function App() {
   return (
     <ThemeConsumer>
