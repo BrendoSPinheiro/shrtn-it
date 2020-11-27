935319d5efb9f40edf520cc13d8e5efe0b67261a
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-27 17:33:11 -0300

    feat: added alternativeText in inputForm component

--- web/src/components/InputForm/index.js
+++ web/src/components/InputForm/index.js
@@ -1,13 +1,18 @@
 import * as S from './styles';
-const InputForm = ({ children, type, icon }) => {
+const InputForm = ({ children, type, icon, alternativeText = null }) => {
   return (
     <S.Wrapper>
       <S.Name>
         {children}
-        <S.InputLabel>
-          {icon} <S.Input type={type} />
-        </S.InputLabel>
+        {!!alternativeText && (
+          <S.AlternativeText href={alternativeText.url}>
+            {alternativeText.name}
+          </S.AlternativeText>
+        )}
       </S.Name>
+      <S.InputLabel>
+        {icon} <S.Input type={type} />
+      </S.InputLabel>
     </S.Wrapper>
   );
 };
