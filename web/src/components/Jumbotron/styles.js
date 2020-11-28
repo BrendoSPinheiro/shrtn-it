178a68bd5397cd079ebe798ddb5490464b5e81a0
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-28 15:55:45 -0300

    feat: added scrollbar custom

--- web/src/components/Jumbotron/styles.js
+++ web/src/components/Jumbotron/styles.js
@@ -16,5 +16,27 @@ export const Wrapper = styled.div`
 
     box-shadow: 10px 129px 62px rgba(0, 0, 0, 0.027476),
       10px 44.6328px 20.5416px rgba(0, 0, 0, 0.0403763);
+
+    ::-webkit-scrollbar-button,
+    ::-webkit-resizer,
+    ::-webkit-scrollbar-corner {
+      display: block;
+    }
+    ::-webkit-scrollbar-track {
+      background: ${theme.colors.background.jumboBg};
+    }
+    ::-webkit-scrollbar {
+      width: 0.5rem;
+      margin: 6rem;
+    }
+    ::-webkit-scrollbar-thumb {
+      background: ${theme.colors.background.jumboBg};
+      border-radius: 8rem;
+    }
+    &:hover {
+      ::-webkit-scrollbar-thumb {
+        background: ${theme.colors.stroke.secondary};
+      }
+    }
   `}
 `;
