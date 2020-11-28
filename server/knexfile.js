2a77601e5f75e77c24b93bce45490f43edb0f8eb
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-11-28 09:54:46 -0300

    chore: remove seeds path

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,7 +12,4 @@ module.exports = {
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
-  seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-  },
 };
