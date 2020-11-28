39321947c277a10cce4138d1c3570ced2f4f3924
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-28 14:16:57 -0300

    fix: width

--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -18,7 +18,7 @@ export const Header = styled.header`
 `;
 export const BorderBottom = styled.div`
   ${({ theme }) => css`
-    width: 100vw;
+    width: 100%;
     height: 3rem;
     background-color: ${theme.colors.background.secondBg};
 
