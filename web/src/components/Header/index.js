78deeaeb26819c2e5226302b247f871742e83936
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-11-30 15:13:00 -0300

    feat: added leftDashBoard

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,18 +1,29 @@
 import * as S from './styles';
 import ToggleTheme from '../../components/ToggleTheme';
 
+import { useHistory } from 'react-router-dom';
+
 import { FiLogOut as LogOutIcon } from 'react-icons/fi';
 
-const Header = () => (
-  <S.Wrapper>
-    <S.Header>
-      <ToggleTheme />
-      <S.Button>
-        <LogOutIcon size={24} />
-      </S.Button>
-    </S.Header>
-    <S.BorderBottom />
-  </S.Wrapper>
-);
+const Header = () => {
+  const history = useHistory();
+
+  const handleLeftDashboard = () => {
+    localStorage.removeItem('@shrtn.it:user');
+
+    history.push('/login');
+  };
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <ToggleTheme />
+        <S.Button onClick={handleLeftDashboard}>
+          <LogOutIcon size={24} />
+        </S.Button>
+      </S.Header>
+      <S.BorderBottom />
+    </S.Wrapper>
+  );
+};
 
 export default Header;
