7f2d365a51af179b1f027260870e9e44d8616887
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-14 15:20:13 -0300

    feat: update date return

--- server/src/app/dto/UrlDto.js
+++ server/src/app/dto/UrlDto.js
@@ -1,3 +1,5 @@
+const { format } = require('date-fns');
+
 class UrlView {
   render(url) {
     return {
@@ -5,9 +7,9 @@ class UrlView {
       title: url.title,
       short_url: `http://localhost:3001/${url.slug}`,
       clicks: url.count_click,
-      created_at: url.created_at,
-      start_expires_date: url.start_expires_date,
-      end_expires_date: url.end_expires_date,
+      created_at: format(url.created_at, 'dd-MM-yyyy'),
+      start_expires_date: format(url.start_expires_date, 'dd-MM-yyyy'),
+      end_expires_date: format(url.end_expires_date, 'dd-MM-yyyy'),
     };
   }
 
