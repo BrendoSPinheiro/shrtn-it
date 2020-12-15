12df4e8e255b8986a82728cca1ea76ae72ff0c69
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-15 19:25:18 -0300

    feat: add port

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const routes = require('./routes');
 require('dotenv/config');
 
+const PORT = process.env.PORT || 3001;
+
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3001, () => {
+server.listen(PORT, () => {
   console.log('🔥 Server started at http://localhost:3001');
 });
