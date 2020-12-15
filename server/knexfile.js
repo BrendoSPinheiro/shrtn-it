0bee8d4fd1cc7c6ce573864e96a85d524ce002d9
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-15 19:00:41 -0300

    feat: add dotenv variables and deploy

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'root',
-    password: 'root',
-    database: 'shrtn_it',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
