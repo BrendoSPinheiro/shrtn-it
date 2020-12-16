9d9cd1ccbbcaa5bcac43809bb3be6016b748352d
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-12-15 23:07:37 -0300

    feat: alterd route the api

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,13 +1,18 @@
 const path = require('path');
 
+console.log('USERRRR', process.env.DB_USER);
+console.log('PORTT', process.env.DB_PORT);
+console.log('HOST', process.env.DB_HOST);
+console.log('DB', process.env.DB_DATABASE);
+console.log('PASSWORD', process.env.DB_PASSWORD);
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'root',
-    password: 'root',
-    database: 'shrtn_it',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const routes = require('./routes');
-require('dotenv/config');
 
 const server = express();
 
