7971b26fcd4a444c26ede1798491c3957054a7f5
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-15 23:06:32 -0300

    add origin in cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 require('dotenv').config();
 const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
+
 const server = express();
 
-server.use(cors());
+server.use(cors({
+  origin: 'https://shrtn-it.vercel.app/',
+}));
 server.use(express.json());
 server.use(routes);
 
-server.listen(3001, () => {
-  console.log('🔥 Server started at http://localhost:3001');
+server.listen(PORT, () => {
+  console.log('🔥 Server started');
 });
